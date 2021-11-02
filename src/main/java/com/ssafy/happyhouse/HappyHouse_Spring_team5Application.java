fd97979f86e18db92d54750cbab5c568b252b3b2
이지우 <ljiwoo59@gmail.com>
2021-11-02 13:19:13 +0900

    init

---
+++ src/main/java/com/ssafy/happyhouse/HappyHouse_Spring_team5Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.happyhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyHouse_Spring_team5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyHouse_Spring_team5Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/ssafy/happyhouse/controller/HappyHouseMapController.java
@@ -0,0 +1,43 @@
+package com.ssafy.happyhouse.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.happyhouse.model.HouseInfoDto;
+import com.ssafy.happyhouse.model.SidoGugunCodeDto;
+import com.ssafy.happyhouse.model.service.HappyHouseMapService;
+
+@RestController
+@RequestMapping("/map")
+public class HappyHouseMapController {
+
+	@Autowired
+	private HappyHouseMapService service;
+	
+	@GetMapping("/sido")
+	public ResponseEntity<List<SidoGugunCodeDto>> sido() throws Exception {
+		return new ResponseEntity<List<SidoGugunCodeDto>>(service.getSido(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/gugun")
+	public ResponseEntity<List<SidoGugunCodeDto>> gugun(@RequestParam("sido") String sido) throws Exception {
+		return new ResponseEntity<List<SidoGugunCodeDto>>(service.getGugunInSido(sido), HttpStatus.OK);
+	}
+	
+	@GetMapping("/dong")
+	public ResponseEntity<List<HouseInfoDto>> dong(@RequestParam("gugun") String gugun) throws Exception {
+		return new ResponseEntity<List<HouseInfoDto>>(service.getDongInGugun(gugun), HttpStatus.OK);
+	}
+	
+	@GetMapping("/apt")
+	public ResponseEntity<List<HouseInfoDto>> apt(@RequestParam("dong") String dong) throws Exception {
+		return new ResponseEntity<List<HouseInfoDto>>(service.getAptInDong(dong), HttpStatus.OK);
+	}
+}
